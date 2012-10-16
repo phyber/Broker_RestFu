35a47e283476b08c24cb40344a9d4402b8a9fd1d
phyber <phyber@localhost>
2012-10-16 09:58:39 +0100

    Add enUS locale.

---
+++ Locales/enUS.lua
@@ -0,0 +1,19 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("Broker_RestFu", "enUS", true)
+if not L then return end
+
+-- Tooltip 
+L["Time played"] = true
+L["Last played"] = true
+L["Time to rest"] = true
+L["Current XP"] = true
+L["Rest XP"] = true
+L["Zone"] = true
+L["Fully rested"] = true
+L["Unknown"] = true
+L["Total time played"] = true
+L["%s |cffffffffago|r"] = true
+
+-- Settings
+L["Filter"] = true
+L["Purge"] = true
+L["Purge Characters"] = true
