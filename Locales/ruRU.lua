e6a9aa0aa3ecdc3cb7231f19324db2a5c6b8ae32
phyber <phyber@localhost>
2012-10-16 10:30:03 +0100

    Add ruRU translation. Some strings not yet translated.

---
+++ Locales/ruRU.lua
@@ -0,0 +1,31 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("Broker_RestFu", "ruRU")
+if not L then return end
+
+-- Tooltip 
+L["Time Played"] = "Время игры"
+--L["Last Played"] = true
+L["Time to Rest"] = "Время до полного отдыха"
+L["Current XP"] = "Текущий опыт"
+L["Rest XP"] = "Отдохнувший опыт"
+L["Zone"] = "Область"
+L["Fully rested"] = "Полностью отдохнул"
+L["Unknown"] = "Область"
+L["Total time played"] = "Общее время игры"
+--L["ago"] = true
+
+-- Settings
+L["Filter"] = "Фильтр"
+--L["Filter specific characters or realms to hide them from the tooltip."] = true
+--L["Filter Realm"] = true
+--L["Select a realm to filter"] = true
+--L["Filter Character from %s"] = true
+--L["Select a character to filter"] = true
+L["Purge"] = "Очистить"
+--L["Purge characters or realms"] = true
+--L["Purge Realm"] = true
+--L["Select a realm to purge"] = true
+--L["Are you sure you wish to delete '%s'?"] = true
+--L["Purge Character from %s"] = true
+--L["Select a character to purge"] = true
+--L["Minimap Icon"] = true
+--L["Toggle minimap icon"] = true
