129ab734b3b554d8c254963befa6275dbb088367
phyber <phyber@localhost>
2012-10-16 10:26:16 +0100

    Added deDE translation. Some strings not yet translated.

---
+++ Locales/deDE.lua
@@ -0,0 +1,31 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("Broker_RestFu", "deDE")
+if not L then return end
+
+-- Tooltip 
+L["Time Played"] = "Spielzeit"
+L["Last Played"] = 
+L["Time to Rest"] = "\195\156brige Erholungszeit"
+L["Current XP"] = "Aktuelle EP"
+L["Rest XP"] = "Erholungs-EP"
+L["Zone"] = "Zone"
+L["Fully rested"] = "vollst\195\164ndig erholt"
+L["Unknown"] = "Unbekannt"
+L["Total time played"] = "Gesamte Spielzeit"
+L["ago"] = "ago"
+
+-- Settings
+L["Filter"] = "Filter"
+--L["Filter specific characters or realms to hide them from the tooltip."] = true
+--L["Filter Realm"] = true
+--L["Select a realm to filter"] = true
+--L["Filter Character from %s"] = true
+--L["Select a character to filter"] = true
+L["Purge"] = "Purge"
+--L["Purge characters or realms"] = true
+--L["Purge Realm"] = true
+--L["Select a realm to purge"] = true
+--L["Are you sure you wish to delete '%s'?"] = true
+--L["Purge Character from %s"] = true
+--L["Select a character to purge"] = true
+L["Minimap Icon"] = "Minikarte Symbol"
+L["Toggle minimap icon"] = "Minikarte Symbol An/Aus"
